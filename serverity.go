1853a28c137c4d25a650920bd8c18828e0488552
MAO Hieng <maohieng@gmail.com>
2022-08-08 13:35:34 +0700

    Add severe panic instead of warning

--- serverity.go
+++ serverity.go
@@ -1,5 +1,13 @@
 package errs
 
+type level byte
+
+const (
+	levelWarn level = 3 << iota
+	levelError
+	levelPanic
+)
+
 // Severity is borrowed from go-kit/log's level
 type Severity interface {
 	String() string
@@ -12,12 +20,8 @@ func SevereError() Severity { return errorValue }
 // SevereWarn returns the unique value added to error events by Warn.
 func SevereWarn() Severity { return warnValue }
 
-type level byte
-
-const (
-	levelWarn level = 3 << iota
-	levelError
-)
+// SeverePanic returns the unique value added to error events by Panic.
+func SeverePanic() Severity { return panicValue }
 
 type levelValue struct {
 	name string
@@ -26,7 +30,10 @@ type levelValue struct {
 
 var (
 	errorValue = &levelValue{level: levelError, name: "error"}
+	// warnValue is deprecated
+	// Deprecated: no more usage and will be removed in the next release
 	warnValue  = &levelValue{level: levelWarn, name: "warn"}
+	panicValue = &levelValue{level: levelPanic, name: "panic"}
 )
 
 func (v *levelValue) String() string { return v.name }
